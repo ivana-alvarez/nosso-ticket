10a3ce1f31f1843e6715934e07205a7be9d62b8c
ivana <i.alvarez@rsegrp.com>
2022-02-10 16:44:57 -0400

    cambios en el landing

--- src/views/pages/authentication/landing/CarouselImg.tsx
+++ src/views/pages/authentication/landing/CarouselImg.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react';
-import { useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
+import * as React from 'react'
+import { useTheme } from '@mui/material/styles'
+import Box from '@mui/material/Box'
 // import MobileStepper from '@mui/material/MobileStepper';
 // import Paper from '@mui/material/Paper';
 // import Typography from '@mui/material/Typography';
@@ -8,63 +8,59 @@ import Box from '@mui/material/Box';
 // import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
 // import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 
-import home from '../../../../assets/images/landing/LOGOS_METROS_BLANCOS_Barinas.png';
-import home_2 from '../../../../assets/images/landing/LOGOS_METROS_BLANCOS-02.png';
-import home_4 from '../../../../assets/images/landing/LOGOS_METROS_BLANCOS-0422.png';
-import home_5 from '../../../../assets/images/landing/LOGOS_METROS_BLANCOS-05.png';
-import SwipeableViews from 'react-swipeable-views';
-import { autoPlay } from 'react-swipeable-views-utils';
+import metroccs from '../../../../assets/images/landing/metroccs.png'
+// import transaragua from '../../../../assets/images/landing/transaragua.png'
+import valencia from '../../../../assets/images/landing/valencia.png'
+import ife from '../../../../assets/images/landing/ife.png'
+import barinas from '../../../../assets/images/landing/barinas.png'
+import SwipeableViews from 'react-swipeable-views'
+import { autoPlay } from 'react-swipeable-views-utils'
 
-const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews)
 
 const images = [
-  // {
-  //   label: 'San Francisco – Oakland Bay Bridge, United States',
-  //   imgPath:'https://www.bing.com/images/search?view=detailV2&ccid=hj9jN3hu&id=B531B59A2C47E2EB9DDBAD91BB089F4ED977B73C&thid=OIP.hj9jN3huKVAK8FxC4vAI6gHaDt&mediaurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.863f6337786e29500af05c42e2f008ea%3frik%3dPLd32U6fCLuRrQ%26riu%3dhttp%253a%252f%252fwww.caf.net%252fimg%252fall%252fproductos_servicios%252fsoluciones_integrales%252fcasos_estudio%252fmetros%252fcaracas%252fmetro-caracas-destacado.jpg%26ehk%3dSJJedIJt2V5Xyz5vKY9B%252bJscIwJqmiD4Tqn5G8iDUm0%253d%26risl%3d%26pid%3dImgRaw%26r%3d0%26sres%3d1%26sresct%3d1%26srh%3d651%26srw%3d1300&exph=389&expw=776&q=metro+caracas&simid=608004542827216813&FORM=IRPRST&ck=9E0E7933F04B20D991ACE0476C861CF8&selectedIndex=1&ajaxhist=0&ajaxserp=0' ,
-      
-  // },
-  {
-    index:0,
-    label: 'San Francisco – Oakland Bay Bridge, United States',
-    imgPath: home,
-  },
-  {
-    index:1,
-    label: 'Bird',  
-    imgPath: home_2,
-  },
-  {
-    index:2,
-    label: 'Bali, Indonesia',
-    imgPath: home_4,
-  },
-  {
-    index:3,
-    label: 'Goč, Serbia',
-    imgPath: home_5,
-  },
-];
+    {
+        index: 0,
+        label: 'metro de caracas',
+        imgPath: metroccs,
+    },
+    {
+        index: 1,
+        label: 'metro de valencia',
+        imgPath: valencia,
+    },
+    {
+        index: 2,
+        label: 'sistema ferrroviario',
+        imgPath: ife,
+    },
+    {
+        index: 3,
+        label: 'bus barinas',
+        imgPath: barinas,
+    },
+]
 
 const CarouselImg = () => {
-  const theme = useTheme();
-  const [activeStep, setActiveStep] = React.useState(0);
-//   const maxSteps = images.length;
+    const theme = useTheme()
+    const [activeStep, setActiveStep] = React.useState(0)
+    //   const maxSteps = images.length;
 
-//   const handleNext = () => {
-//     setActiveStep((prevActiveStep) => prevActiveStep + 1);
-//   };
+    //   const handleNext = () => {
+    //     setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    //   };
 
-//   const handleBack = () => {
-//     setActiveStep((prevActiveStep) => prevActiveStep - 1);
-//   };
+    //   const handleBack = () => {
+    //     setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    //   };
 
-  const handleStepChange = (step: number) => {
-    setActiveStep(step);
-  };
+    const handleStepChange = (step: number) => {
+        setActiveStep(step)
+    }
 
-  return (
-    <Box sx={{ maxWidth: 750, flexGrow: 1, }}>
-      {/* <Paper
+    return (
+        <Box sx={{ maxWidth: 750, flexGrow: 1 }}>
+            {/* <Paper
         square
         elevation={0}
         sx={{
@@ -77,37 +73,37 @@ const CarouselImg = () => {
       >
         <Typography>{images[activeStep].label}</Typography>
       </Paper> */}
-      <AutoPlaySwipeableViews
-        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-        index={activeStep}
-        onChangeIndex={handleStepChange}
-        enableMouseEvents
-        // sx={{
-        //     marginTop:10,
-            
-        //   }}
-      >
-         {images.map(({ imgPath , label , index}) => (
-          <div key={label}>
-            {Math.abs(activeStep - index) <= 2 ? (
-              <Box
-                component="img"
-                sx={{
-                    top:10,
-                  height: 400,
-                  display: 'block',
-                  maxWidth: 900,
-                  overflow: 'hidden',
-                  width: '100%',
-                }}
-                src={imgPath}
-                alt={label}
-              />
-            ) : null}
-          </div>
-        ))}
-      </AutoPlaySwipeableViews>
-        {/* </Box><MobileStepper 
+            <AutoPlaySwipeableViews
+                axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+                index={activeStep}
+                onChangeIndex={handleStepChange}
+                enableMouseEvents
+                // sx={{
+                //     marginTop:10,
+
+                //   }}
+            >
+                {images.map(({ imgPath, label, index }) => (
+                    <div key={label}>
+                        {Math.abs(activeStep - index) <= 2 ? (
+                            <Box
+                                component="img"
+                                sx={{
+                                    top: 10,
+                                    height: 400,
+                                    display: 'block',
+                                    maxWidth: 900,
+                                    overflow: 'hidden',
+                                    width: '100%',
+                                }}
+                                src={imgPath}
+                                alt={label}
+                            />
+                        ) : null}
+                    </div>
+                ))}
+            </AutoPlaySwipeableViews>
+            {/* </Box><MobileStepper 
          steps={maxSteps} 
          position="static" 
          activeStep={activeStep}
@@ -136,8 +132,8 @@ const CarouselImg = () => {
            </Button>
          }
       />*/}
-    </Box> 
-  );
+        </Box>
+    )
 }
 
-export default CarouselImg;
+export default CarouselImg
--- src/views/pages/authentication/products/Demos.tsx
+++ src/views/pages/authentication/products/Demos.tsx
@@ -1,120 +1,180 @@
 // import { Link } from 'react-router-dom';
 
 // material-ui
-import { makeStyles } from '@material-ui/styles';
-import { 
+import { makeStyles } from '@material-ui/styles'
+import {
     // Button,
-    // ButtonBase, 
-    Container, 
-    Grid, 
-    Typography, 
-    Theme, 
+    // ButtonBase,
+    Container,
+    Grid,
+    Typography,
+    Theme,
     Box,
     Divider,
-} from '@material-ui/core';
-import { useTheme } from '@material-ui/core/styles';
+} from '@material-ui/core'
+import { useTheme } from '@material-ui/core/styles'
 
 // project imports
-import FadeInWhenVisible from './Animation';
+import FadeInWhenVisible from './Animation'
 // import AnimateButton from 'ui-component/extended/AnimateButton';
-import { gridSpacing } from 'store/constant';
+import { gridSpacing } from 'store/constant'
 
 // assets
-import imgDemo1 from 'assets/images/landing/tarjtas_landing_Mesa_de_trabajo_1.png';
-import imgDemo2 from 'assets/images/landing/tarjtas_landing-02.png';
-import imgDemo3 from 'assets/images/landing/tarjtas_landing-03.png';
-import imgDemo4 from 'assets/images/landing/tarjtas_landing-04.png';
-import imgDemo5 from 'assets/images/landing/tarjtas_landing-05.png';
+import imgDemo1 from 'assets/images/landing/tarjtas_landing_Mesa_de_trabajo_1.png'
+import imgDemo2 from 'assets/images/landing/tarjtas_landing-02.png'
+import imgDemo3 from 'assets/images/landing/tarjtas_landing-03.png'
+import imgDemo4 from 'assets/images/landing/tarjtas_landing-04.png'
+import imgDemo5 from 'assets/images/landing/tarjtas_landing-05.png'
 
 // style constant
 const useStyles = makeStyles((theme: Theme) => ({
     demoImage: {
         width: '100%',
-        borderRadius: '12px'
+        borderRadius: '12px',
     },
-    divide:{
+    divide: {
         height: '20px',
-    }
-}));
+    },
+}))
 
 // ==============================|| LANDING - DEMOS PAGE ||============================== //
 
 const DemosPage = () => {
-    const classes = useStyles();
-    const theme = useTheme();
+    const classes = useStyles()
+    const theme = useTheme()
     return (
         <Container>
             <Grid container spacing={gridSpacing}>
                 <Grid item xs={12} lg={5} md={10}>
-                    <Grid container spacing={2} sx={{ mb: '16px' }}>
+                    <Grid container spacing={2}>
                         <Grid item xs={12}>
                             <Grid container spacing={1}>
                                 <Grid item>
                                     <Typography variant="h1" color="primary">
                                         Productos
                                     </Typography>
-                                    <Divider  />
+                                    <Divider />
                                 </Grid>
                             </Grid>
                         </Grid>
                         <Grid item xs={12}>
                             <Typography variant="h2" component="div">
-                               Personaliza tu tarjeta 
-                               <Box component="span" sx={{ ml: 2, color: theme.palette.primary.main }}>T-TICKET </Box>
-                               para que se adapte a tus necesidades de viaje.
+                                Personaliza
+                                <Box
+                                    component="span"
+                                    sx={{
+                                        ml: 2,
+                                        color: theme.palette.primary.main,
+                                    }}
+                                >
+                                    T-TICKET{' '}
+                                </Box>
+                                para que se adapte a tus necesidades de viaje.
                             </Typography>
                         </Grid>
-                        
                     </Grid>
                 </Grid>
                 <Grid item xs={12}>
-                    <Grid container justifyContent="center" spacing={gridSpacing} sx={{ textAlign: 'center' }}>
-                        <Grid item md={4} sm={6}>
+                    <Grid
+                        container
+                        justifyContent="center"
+                        spacing={gridSpacing}
+                        sx={{ textAlign: 'center' }}
+                    >
+                        <Grid
+                            item
+                            md={4}
+                            sm={6}
+                            className="animate-pulse duration-1000"
+                        >
                             <FadeInWhenVisible>
                                 {/* <ButtonBase component={Link} to="/dashboard/default" target="_blank"> */}
-                                    <img src={imgDemo1} alt="Berry Dashboard" className={classes.demoImage} />
+                                <img
+                                    src={imgDemo1}
+                                    alt="Berry Dashboard"
+                                    className={classes.demoImage}
+                                />
                                 {/* </ButtonBase> */}
                             </FadeInWhenVisible>
                         </Grid>
-                        <Grid item md={4} sm={6}>
+                        <Grid
+                            item
+                            md={4}
+                            sm={6}
+                            className="animate-pulse duration-1000"
+                        >
                             <FadeInWhenVisible>
                                 {/* <ButtonBase component={Link} to="/user/social-profile/posts" target="_blank"> */}
-                                    <img src={imgDemo2} alt="Berry Social App" className={classes.demoImage} />
+                                <img
+                                    src={imgDemo2}
+                                    alt="Berry Social App"
+                                    className={classes.demoImage}
+                                />
                                 {/* </ButtonBase> */}
                             </FadeInWhenVisible>
                         </Grid>
-                        <Grid item md={4} sm={6}>
+                        <Grid
+                            item
+                            md={4}
+                            sm={6}
+                            className="animate-pulse duration-1000"
+                        >
                             <FadeInWhenVisible>
                                 {/* <ButtonBase component={Link} to="/app/mail" target="_blank"> */}
-                                    <img src={imgDemo3} alt="Berry Mail App" className={classes.demoImage} />
+                                <img
+                                    src={imgDemo3}
+                                    alt="Berry Mail App"
+                                    className={classes.demoImage}
+                                />
                                 {/* </ButtonBase> */}
                             </FadeInWhenVisible>
                         </Grid>
                     </Grid>
                 </Grid>
                 <Grid item xs={12}>
-                    <Grid container justifyContent="center" spacing={gridSpacing} sx={{ textAlign: 'center' }}>
-                        <Grid item md={4} sm={6}>
+                    <Grid
+                        container
+                        justifyContent="center"
+                        spacing={gridSpacing}
+                        sx={{ textAlign: 'center' }}
+                    >
+                        <Grid
+                            item
+                            md={4}
+                            sm={6}
+                            className="animate-pulse duration-1000"
+                        >
                             <FadeInWhenVisible>
                                 {/* <ButtonBase component={Link} to="/dashboard/default" target="_blank"> */}
-                                    <img src={imgDemo4} alt="Berry Dashboard" className={classes.demoImage} />
+                                <img
+                                    src={imgDemo4}
+                                    alt="Berry Dashboard"
+                                    className={classes.demoImage}
+                                />
                                 {/* </ButtonBase> */}
                             </FadeInWhenVisible>
                         </Grid>
-                        <Grid item md={4} sm={6}>
+                        <Grid
+                            item
+                            md={4}
+                            sm={6}
+                            className="animate-pulse duration-1000"
+                        >
                             <FadeInWhenVisible>
                                 {/* <ButtonBase component={Link} to="/user/social-profile/posts" target="_blank"> */}
-                                    <img src={imgDemo5} alt="Berry Social App" className={classes.demoImage} />
+                                <img
+                                    src={imgDemo5}
+                                    alt="Berry Social App"
+                                    className={classes.demoImage}
+                                />
                                 {/* </ButtonBase> */}
                             </FadeInWhenVisible>
                         </Grid>
-                        
                     </Grid>
                 </Grid>
-            
             </Grid>
         </Container>
-    );
-};
+    )
+}
 
-export default DemosPage;
+export default DemosPage
