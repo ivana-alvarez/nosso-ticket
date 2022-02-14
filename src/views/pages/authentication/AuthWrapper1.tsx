146d99a7475ea479e6c2a9cb3373e58e714a4de9
ivana <i.alvarez@rsegrp.com>
2022-02-14 16:55:58 -0400

    login

--- src/views/pages/authentication/AuthWrapper1.tsx
+++ src/views/pages/authentication/AuthWrapper1.tsx
@@ -6,7 +6,7 @@ import { styled } from '@material-ui/core/styles'
 const AuthWrapper1 = styled('div')(({ theme }) => ({
     backgroundColor:
         theme.palette.mode === 'dark'
-            ? theme.palette.background.paper
+            ? theme.palette.background.default
             : theme.palette.primary.light,
     minHeight: '100vh',
 }))
